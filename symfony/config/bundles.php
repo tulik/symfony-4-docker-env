f8e0290ee25549e47c5386c4d4c3ea64994a8dd7
Lukasz Tulikowski <lukasz.tulikowski@savewatersavemoney.com>
2018-10-05 20:47:20 +0100

    Add platform.sh

--- symfony/config/bundles.php
+++ symfony/config/bundles.php
@@ -2,7 +2,5 @@
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
-    Doctrine\Bundle\DoctrineCacheBundle\DoctrineCacheBundle::class => ['all' => true],
-    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
-    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
+    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
 ];

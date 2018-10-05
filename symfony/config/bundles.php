85ea2bdd9c9da37ca9549997fe9c3780631ea488
Lukasz Tulikowski <lukasz.tulikowski@savewatersavemoney.com>
2018-10-05 20:59:40 +0100

    Add blackfire integration

--- symfony/config/bundles.php
+++ symfony/config/bundles.php
@@ -3,4 +3,7 @@
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
+    Doctrine\Bundle\DoctrineCacheBundle\DoctrineCacheBundle::class => ['all' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
+    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
 ];
